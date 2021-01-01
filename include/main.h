ef48bd70de4f079880f839acceab3ddef9d0a60c
Al-Azif <alazif@miskatonic.io>
2021-01-01 14:23:56 -0800

    Working on trophy stuff

--- include/main.h
+++ include/main.h
@@ -8,12 +8,45 @@
 #define SPLIT_APP 1
 #define SPLIT_PATCH 2
 
+#define TROPHY_MAGIC 0xDCA24D00
+#define NPBIND_MAGIC 0xD294A018
+
+#define bswap16 __builtin_bswap16
+#define bswap32 __builtin_bswap32
+#define bswap64 __builtin_bswap64
+
 typedef struct {
   int split;
   int notify;
   int shutdown;
 } configuration;
 
-extern configuration config;
+struct npbind_header {
+  uint32_t magic;
+  uint32_t version;
+  uint64_t file_size;
+  uint64_t entry_size;
+  uint64_t num_entries;
+  unsigned char padding[96];
+};
+
+struct npbind_entry {
+  uint16_t type;
+  uint16_t size;
+  unsigned char data[];
+};
+
+struct npbind_body {
+  struct npbind_entry npcommid_name;
+  struct npbind_entry tophy_number;
+  struct npbind_entry unk1;
+  struct npbind_entry unk2;
+  unsigned char padding[152];
+};
+
+struct npbind_file {
+  struct npbind_header header;
+  struct npbind_body body[];
+};
 
 #endif
--- source/main.c
+++ source/main.c
@@ -42,14 +42,55 @@ static int config_handler(void *user, const char *name, const char *value) {
   return 1;
 }
 
-void dump_game(char *title_id, char *usb_path) {
-  char base_path[64];
-  char src_path[64];
-  char dst_file[64];
-  char dst_app[64];
-  char dst_pat[64];
-  char dump_sem[64];
-  char comp_sem[64];
+int npbind_parse(const char *filename) {
+  struct npbind_header header;
+  memset_s(&header, sizeof(struct npbind_header), 0, sizeof(struct npbind_header));
+
+  int fd = open (filename, O_RDONLY, 0);
+  if (fd < 0)  {
+    return 1;
+  }
+
+  if (sizeof(struct npbind_header) != read(fd, &header, sizeof(struct npbind_header))) {
+    close(fd);
+    return 2;
+  }
+
+  header.magic = bswap32(header.magic);
+  header.version = bswap32(header.version);
+  header.file_size = bswap64(header.file_size);
+  header.entry_size = bswap64(header.entry_size);
+  header.num_entries = bswap64(header.num_entries);
+  // DEBUG: printf_notification("Magic: 0x%08X\nVersion: %u\nFileize: %u bytes\nEntry Size: %u bytes\nNum of Entries: %u", header.magic, header.version, header.file_size, header.entry_size, header.num_entries);
+
+  if (header.magic != NPBIND_MAGIC) {
+    close(fd);
+    return 3;
+  }
+
+  // --- HEADER BUILT AND CORRECT ENDIAN SET ----------------------------------
+
+  // 0x14 is the size of the digest that is tacked onto the end of the file
+  int num_bodies = (header.file_size - sizeof(struct npbind_header) - 0x14) /  header.entry_size;
+
+  struct npbind_body body;
+  memset_s(&body, header.entry_size, 0, header.entry_size);
+  while((int64_t)header.entry_size == read(fd, &body, header.entry_size)) {
+    printf_notification("%s", body.npcommid_name.data);
+  }
+
+  close(fd);
+  return 0;
+}
+
+void dump_app(char *title_id, char *usb_path) {
+  char base_path[64] = {0};
+  char src_path[64] = {0};
+  char dst_file[64] = {0};
+  char dst_app[64] = {0};
+  char dst_pat[64] = {0};
+  char dump_sem[64] = {0};
+  char comp_sem[64] = {0};
 
   sprintf(base_path, "%s/%s", usb_path, title_id);
 
@@ -143,10 +184,10 @@ void dump_game(char *title_id, char *usb_path) {
 int _main(struct thread *td) {
   UNUSED(td);
 
-  char title_id[64];
-  char usb_name[64];
-  char usb_path[64];
-  char cfg_path[64];
+  char title_id[64] = {0};
+  char usb_name[64] = {0};
+  char usb_path[64] = {0};
+  char cfg_path[64] = {0};
 
   initKernel();
   initLibc();
@@ -179,20 +220,24 @@ int _main(struct thread *td) {
   sprintf(cfg_path, "%s/dumper.cfg", usb_path);
   cfg_parse(cfg_path, config_handler, &config);
 
-  if (!wait_for_game(title_id)) {
-    sprintf(notify_buf, "Waiting for game to launch...");
+  if (!wait_for_app(title_id)) {
+    sprintf(notify_buf, "Waiting for application to launch...");
     do {
       sceKernelSleep(1);
-    } while (!wait_for_game(title_id));
+    } while (!wait_for_app(title_id));
     notify_buf[0] = '\0';
   }
+
+  // DEBUG: char npbindpath[64] = {0};
+  // DEBUG: sprintf(npbindpath, "/system_data/priv/appmeta/%s/npbind.dat", title_id);
+  // DEBUG: npbind_parse(npbindpath);
 
   if (wait_for_bdcopy(title_id) < 100) {
     int progress;
     do {
       sceKernelSleep(1);
       progress = wait_for_bdcopy(title_id);
-      sprintf(notify_buf, "Waiting for game to copy\n%i%% completed...", progress);
+      sprintf(notify_buf, "Waiting for application to copy\n%i%% completed...", progress);
     } while (progress < 100);
     notify_buf[0] = '\0';
   }
@@ -200,7 +245,7 @@ int _main(struct thread *td) {
   printf_notification("Start dumping\n%s to %s", title_id, usb_name);
   sceKernelSleep(5);
 
-  dump_game(title_id, usb_path);
+  dump_app(title_id, usb_path);
 
   if (config.shutdown) {
     printf_notification("%s dumped.\nShutting down...", title_id);
